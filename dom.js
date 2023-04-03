17cdd465e32bf38b4484959d8ba83c92af4a89de
Lakshya000007 <lakshya719@gmail.com>
2023-04-03 13:44:15 +0530

    commit 6

--- dom.js
+++ dom.js
@@ -24,3 +24,41 @@ li[4].classList.add("list-group-item");
 document.querySelector("li:nth-child(2)").style.backgroundColor = "green";
 document.querySelector("li:nth-child(3)").style.display = "none";
 console.log("Hello");
+
+console.log(document.querySelector("#items").parentElement);
+console.log(
+  (document.querySelector("#items").lastElementChild.style.backgroundColor =
+    "blue")
+);
+
+console.log(
+  (document.querySelector("#items").firstElementChild.style.backgroundColor =
+    "blue")
+);
+
+console.log(document.querySelector("#items").lastChild);
+console.log(document.querySelector("#items").firstChild);
+
+const ul = document.getElementById("items");
+console.log(ul.nextSibling);
+console.log(ul.previousSibling);
+
+console.log(ul.nextElementSibling);
+console.log(ul.previousElementSibling);
+
+const newElement = document.createElement("span");
+newElement.setAttribute("title", "new");
+console.log(newElement);
+
+const newText = document.createTextNode("New");
+newElement.appendChild(newText);
+console.log(newElement);
+
+const container = document.querySelector("header .container");
+const h1 = document.querySelector("header h1");
+const data1 = document.createElement("div");
+data1.innerHTML = "Hello World";
+console.log(data1);
+container.insertBefore(data1, h1);
+
+ul.insertBefore(data1, li[0]);
