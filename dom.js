36beecc3277a4a146157ef8873fa88ea7ca98134
Lakshya000007 <lakshya719@gmail.com>
2023-04-03 16:08:29 +0530

    commit 8

--- dom.js
+++ dom.js
@@ -18,3 +18,20 @@ for (const data of li) {
 
   data.appendChild(editBtn);
 }
+
+var filter = document.getElementById("filter");
+
+filter.addEventListener("keyup", filterItems);
+
+function filterItems(e) {
+  var text = e.target.value.toLowerCase();
+  var items = ul.getElementsByTagName("li");
+  Array.from(items).forEach(function (item) {
+    var itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
