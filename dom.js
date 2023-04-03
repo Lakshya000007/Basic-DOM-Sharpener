0ca5d39b61a9f3bede81c4c9fe4087264b14578d
Lakshya000007 <lakshya719@gmail.com>
2023-04-03 11:53:13 +0530

    First Commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("header-title");
+title.style.border = "2px solid black";
+title.style.padding = "4px";
+
+const addTitle = document.getElementsByClassName("title");
+console.log(addTitle);
+addTitle[0].style.fontWeight = "900";
+addTitle[0].style.color = "green";
